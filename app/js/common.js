01b98e74c09e879eca3b5f64613946ea17556bcb
Andrii Savchuk <andriksr@gmail.com>
2017-01-13 11:42:38 +0200

    Added script for navbar

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,16 @@
 $(function() {
+    //fixed navbar during scrolling to bottom
+    $(window).scroll(function () {
 
-	// Custom JS
+        if ($(window).scrollTop() > 820) {
+            $('.navbar').addClass('navbar-fixed-top');
+            $('.bg-inverse').css("opacity", "0.8");
+        }
+
+        if ($(window).scrollTop() < 820) {
+            $('.navbar').removeClass('navbar-fixed-top');
+            $('.bg-inverse').css("opacity", "");
+        }
+    })
 
 });
