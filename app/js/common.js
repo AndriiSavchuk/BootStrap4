a299aead7acbdf5593d1bbfed958b06bb643808f
Andrii Savchuk <andriksr@gmail.com>
2017-01-14 11:46:55 +0200

    Added animation to the section Features

--- app/js/common.js
+++ app/js/common.js
@@ -25,6 +25,33 @@ $(function() {
         return false;
     });
 
+    //Animation
+
+    $('.js-wp-1').waypoint(function (direction) {
+         $('.js-wp-1').addClass('animated fadeInDown');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js-wp-2').waypoint(function (direction) {
+        $('.js-wp-2').addClass('animated fadeInLeft');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js-wp-3, .js-wp-4').waypoint(function (direction) {
+        $('.js-wp-3, .js-wp-4').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js-wp-5').waypoint(function (direction) {
+        $('.js-wp-5').addClass('animated fadeInRight');
+    }, {
+        offset: '50%'
+    });
+
+
 
 
 });
